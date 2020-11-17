524249fdb53f95ab92b3980afe6e78c27e526532
Leigh Halliday <leighhalliday@gmail.com>
2020-11-17 08:49:28 -0500

    Starting point

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h2>Fixed</h2>
+      </div>
+
+      <div>
+        <h2>Intrinsic</h2>
+      </div>
+
+      <div>
+        <h2>Responsive</h2>
+      </div>
+
+      <div>
+        <h2>Fill</h2>
+      </div>
+    </div>
+  );
+}
